283d007f9e09b7c8946f5df22d475e7244fec96e
VeroFess <coderzeng@hotmail.com>
2021-07-30 05:41:55 +0800

    Remove #error because it is unnecessary.

--- SharedFiles/Shared.h
+++ SharedFiles/Shared.h
@@ -5,5 +5,4 @@
 #define BAC_VERSION "1.0.6 (OpenSource) by VeroFess (Compiled in " __DATE__ " " __TIME__ ")"
 #endif
 
-#error PLEASE MODIFY BAC_FILTER_ALTITUDE
 #define BAC_FILTER_ALTITUDE L"81638"
